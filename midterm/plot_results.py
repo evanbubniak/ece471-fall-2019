45c8c9fc5955ce08e4d207839d3092638bbcd3e2
Evan Bubniak <evan.bubniak@gmail.com>
2019-10-19 17:13:39 -0400

    added inception python file and put comments for plot_results

---
+++ midterm/plot_results.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# Need input data [training_step_num, average_loss] for each one.
+
+# Plot of learning curves
+# all dots have black outline
+#true labels: blue, with blue square dots
+
+# random labels - red, with red circular dots
+
+# shuffled pixels: green, with green star 5-point star dots
+
+# random pixels: purple, with no dots
+
+# gaussian: black, with black diamond dots
+
+# x-label: thousand steps (steps of 5, from 0 to 25)
+# y-label: average_loss (steps of 0.5, from 0 to 2.5)
+# dots evenly spaced along x axis, with about 1.3 dots per training step, starting at about 0.5
