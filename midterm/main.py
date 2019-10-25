1d2a2375c171157e8df37d98184827ccfe765131
Evan Bubniak <evan.bubniak@gmail.com>
2019-10-25 16:44:59 -0400

    set up loop for all models

--- midterm/main.py
+++ midterm/main.py
@@ -26,13 +26,7 @@ X_test = preprocess_input(X_test)
 y_train = preprocess_labels(y_train)
 y_test = preprocess_labels(y_test)
 
-inception_model = MiniInceptionV3(
-    input_shape = X_train.shape[1:],
-    num_labels = 10
-)
-inception_model.compile()
-
-CORRUPTION_TYPE = ["true labels", "random labels", "shuffled pixels", "random pixels", "gaussian"]
+CORRUPTION_TYPE = ["true_labels", "random_labels", "shuffled_pixels", "random_pixels", "gaussian"]
 NUM_STEPS = [10000, 10000, 10000, 10000, 15000]
 true_inputs = [X_train, y_train]
 random_labels = [X_train, randomize_labels(y_train.shape[0], 10)]
@@ -41,6 +35,13 @@ random_pixels = [randomize_pixels(X_train), y_train]
 gaussian = [create_gaussian_noise(X_train), y_train]
 DATA_INPUTS = [true_inputs, random_labels, shuffled_pixels, random_pixels, gaussian]
 
-
-num_epochs = ceil(NUM_STEPS[0] / STEPS_PER_EPOCH)
-inception_model.fit(*DATA_INPUTS[0], X_test, y_test, num_epochs, CORRUPTION_TYPE[0])
+for job_name, step_count, data_input in zip(CORRUPTION_TYPE, NUM_STEPS, DATA_INPUTS):
+    X = data_input[0]
+    y = data_input[1]
+    num_epochs = ceil(step_count / STEPS_PER_EPOCH)
+    inception_model = MiniInceptionV3(
+        input_shape = X.shape[1:],
+        num_labels = 10
+        )
+    inception_model.compile()
+    inception_model.fit(*data_input, X_test, y_test, num_epochs, job_name)
